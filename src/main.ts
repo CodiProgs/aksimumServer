08eaa824a56ee3667730b43ed7bf6d95c0a025db
CodiProgs <samuil.codiWork@mail.ru>
2023-10-30 21:23:48 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as CookieParser from 'cookie-parser';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  })
+  app.use(CookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((acc, err) => {
+          acc[err.property] = Object.values(err.constraints).join(
+            ', ',
+          )
+          return acc;
+        }, {});
+        throw new BadRequestException(formattedErrors);
+      }
+    })
+  )
   await app.listen(5019);
 }
 bootstrap();
---
+++ src/user/user.resolver.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class UserResolver {
+
+    @Query(() => String)
+    hello(){
+        return 'Hello World'
+    }
+
+}
